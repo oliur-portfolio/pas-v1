341d91ff93c8711f2a649a6e6aee07df4dc319ed
Oliur Rahman <oliurr941@gmail.com>
2022-09-26 22:16:22 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Slider Code
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    smartSpeed: 1000,
+    dots: false,
+  });
+});
